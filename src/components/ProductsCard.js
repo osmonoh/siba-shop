b5f8a4c6522343eb18952d6391e0ac95e4020506
Dusan <dusan.chmelko@yahoo.com>
2022-03-23 19:46:03 +0100

    add style: productsCard+fix for imgs of dif sizes

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -23,6 +23,22 @@ const ProductsCard = ({
   const { setProduct } = useContext(MyContext);
 
   return (
+    // <Grid
+    //   item
+    //   xs={4}
+    //   onClick={() => {
+    //     setProduct(item);
+    //     sessionStorage.setItem("product", JSON.stringify(item));
+    //   }}
+    // >
+    //   <Link to={"/item/" + itemId}>
+    //     <img src={picture} alt={displayName} style={{ width: "100%" }} />
+    //     <p>{displayName}</p>
+    //     {/* <p>{originalPrice}</p> */}
+    //     <p>&euro;{currentPrice}</p>
+    //   </Link>
+    // </Grid>
+
     <Grid
       item
       xs={4}
@@ -32,37 +48,41 @@ const ProductsCard = ({
       }}
     >
       <Link to={"/item/" + itemId}>
-        <img src={picture} alt={displayName} style={{ width: "100%" }} />
-        <p>{displayName}</p>
-        {/* <p>{originalPrice}</p> */}
-        <p>&euro;{currentPrice}</p>
+        <Card
+          sx={{
+            maxWidth: 345,
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+          onClick={() => {
+            setProduct(item);
+            sessionStorage.setItem("product", JSON.stringify(item));
+          }}
+        >
+          {/* <Link to={"/item/" + itemId}> */}
+          <CardMedia
+            component="img"
+            // height="100%"
+            image={picture}
+            alt={displayName}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" noWrap>
+              {displayName}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              textAlign="right"
+            >
+              &euro;{currentPrice}
+            </Typography>
+          </CardContent>
+        </Card>
       </Link>
     </Grid>
-
-    // <Card
-    //   sx={{ maxWidth: 345 }}
-    //   onClick={() => {
-    //     setProduct(item);
-    //     sessionStorage.setItem("product", JSON.stringify(item));
-    //   }}
-    // >
-    //   <Link to={"/item/" + itemId}>
-    //     <CardMedia
-    //       component="img"
-    //       // height="140"
-    //       image={picture}
-    //       alt={displayName}
-    //     />
-    //     <CardContent>
-    //       <Typography gutterBottom variant="h5" component="div">
-    //         {displayName}
-    //       </Typography>
-    //       <Typography variant="body2" color="text.secondary">
-    //         &euro;{currentPrice}
-    //       </Typography>
-    //     </CardContent>
-    //   </Link>
-    // </Card>
   );
 };
 
