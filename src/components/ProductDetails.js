747a240895f4039975d37f1f5aa5bbb150c77519
Dusan <dusan.chmelko@yahoo.com>
2022-03-29 21:13:18 +0200

    cleaning code productdetails

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,114 +1,32 @@
 import React, { useContext, useState } from "react";
 import { MyContext } from "../context/MyContext";
 
-import Box from "@mui/material/Box";
-import { Container, Card } from "@mui/material";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-
-import Snackbar from "@mui/material/Snackbar";
-import CloseIcon from "@mui/icons-material/Close";
+import {
+  Box,
+  Container,
+  Stack,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+  IconButton,
+  Snackbar,
+} from "@mui/material";
+import { FavoriteBorder, Close } from "@mui/icons-material";
 
 const ProductDetails = () => {
   const { product } = useContext(MyContext);
-  // const [toCart, setToCart] = useState({});
   const { inCart, setInCart } = useContext(MyContext);
   const { inFav, setInFav } = useContext(MyContext);
 
   const [size, setSize] = useState("");
   const [noSize, setNoSize] = useState(false);
-
   const [open, setOpen] = useState(false);
-
-  // console.log(inCart, inCart.length && typeof inCart[0].amount);
-  // console.log("size------>", size);
-
-  // return (
-  //   <div>
-  //     <img src={product.picture} alt={product.displayName} />
-  //     <p>{product.displayName}</p>
-  //     <select
-  //       name="size"
-  //       id="size"
-  //       value={size}
-  //       onChange={(e) => {
-  //         setSize(e.target.value);
-  //         setNoSize(false);
-  //       }}
-  //     >
-  //       <option value="" defaultValue>
-  //         -
-  //       </option>
-
-  //       {product.availableSizes.map((item) => {
-  //         return (
-  //           <option key={item} value={item}>
-  //             {item}
-  //           </option>
-  //         );
-  //       })}
-  //     </select>
-  //     {noSize && <p>* Please choose size</p>}
-  //     {/* <input type="number" min="1" max="10" /> */}
-  //     <p>{product.description}</p>
-  //     <p>&euro;{product.currentPrice}</p>
-  //     <button
-  //       onClick={() => {
-  //         if (size === "") setNoSize(true);
-  //         else if (
-  //           !inCart.some(
-  //             (item) => product.itemId === item.itemId && size === item.size
-  //           )
-  //         ) {
-  //           setInCart([...inCart, { ...product, size, amount: 1 }]);
-  //         } else {
-  //           setInCart(
-  //             inCart.map((item) => {
-  //               if (product.itemId === item.itemId && size === item.size) {
-  //                 product.amount = item.amount + 1;
-  //                 return { ...product, size };
-  //               } else {
-  //                 return item;
-  //               }
-  //             })
-  //           );
-  //         }
-  //         // setInCart([...inCart, { ...product, size, amount: 1 }])
-  //       }}
-  //     >
-  //       Add to cart
-  //     </button>
-  //     <button
-  //       onClick={() => {
-  //         if (
-  //           !inFav.some((item) => {
-  //             return item.itemId === product.itemId;
-  //           })
-  //         )
-  //           setInFav([...inFav, product]);
-  //         else setInFav(inFav.filter((item) => item.itemId !== product.itemId));
-  //       }}
-  //     >
-  //       {!inFav.some((item) => {
-  //         return item.itemId === product.itemId;
-  //       })
-  //         ? "Add to favourites"
-  //         : "Remove from favourites"}
-  //     </button>
-  //   </div>
-  // );
 
   const onClickToCartBtn = () => {
     if (size === "") setNoSize(true);
@@ -132,7 +50,6 @@ const ProductDetails = () => {
         })
       );
     }
-    // setInCart([...inCart, { ...product, size, amount: 1 }])
   };
 
   const onClickToFavBtn = () => {
@@ -144,10 +61,6 @@ const ProductDetails = () => {
       setInFav([...inFav, product]);
     else setInFav(inFav.filter((item) => item.itemId !== product.itemId));
   };
-
-  // const handleClick = () => {
-  //   setOpen(true);
-  // };
 
   const handleSnackClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -165,7 +78,7 @@ const ProductDetails = () => {
         color="inherit"
         onClick={handleSnackClose}
       >
-        <CloseIcon fontSize="small" />
+        <Close fontSize="small" />
       </IconButton>
     </React.Fragment>
   );
@@ -245,16 +158,6 @@ const ProductDetails = () => {
               </div>
             </div>
 
-            <div>
-              {/* <button onClick={onClickToCartBtn}>Add to cart</button> */}
-              {/* <button onClick={onClickToFavBtn}>
-              {!inFav.some((item) => {
-                return item.itemId === product.itemId;
-              })
-                ? "Add to favourites"
-                : "Remove from favourites"}
-            </button> */}
-            </div>
             <Stack spacing={1} direction="row" margin="0 auto">
               <Button
                 variant="contained"
@@ -266,7 +169,7 @@ const ProductDetails = () => {
               <Button
                 variant="outlined"
                 size="large"
-                endIcon={<FavoriteBorderIcon />}
+                endIcon={<FavoriteBorder />}
                 onClick={onClickToFavBtn}
               >
                 {!inFav.some((item) => {
@@ -277,34 +180,11 @@ const ProductDetails = () => {
               </Button>
             </Stack>
           </CardContent>
-          {/* <div>
-          <select
-            name="size"
-            id="size"
-            value={size}
-            onChange={(e) => {
-              setSize(e.target.value);
-              setNoSize(false);
-            }}
-          >
-            <option value="" defaultValue>
-              -
-            </option>
-            {product.availableSizes.map((item) => {
-              return (
-                <option key={item} value={item}>
-                  {item}
-                </option>
-              );
-            })}
-          </select>
-          {noSize && <p>* Please choose size</p>}
-        </div> */}
         </Card>
 
         <Snackbar
           open={open}
-          autoHideDuration={6000}
+          autoHideDuration={4000}
           onClose={handleSnackClose}
           message="Added to cart"
           action={snackAction}
