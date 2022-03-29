535670dd8c07a4f98c4498fcd80f50b5b4510c04
Dusan <dusan.chmelko@yahoo.com>
2022-03-29 21:19:19 +0200

    code cleaning cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,23 +2,24 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MyContext } from "../context/MyContext";
 
-import Box from "@mui/material/Box";
-import { Container, Stack, Card } from "@mui/material";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CardActions from "@mui/material/CardActions";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import IndeterminateCheckBoxIcon from "@mui/icons-material/IndeterminateCheckBox";
-import AddBoxIcon from "@mui/icons-material/AddBox";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Button from "@mui/material/Button";
-
+import {
+  Container,
+  Stack,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
 import { grey, purple, indigo, blue } from "@mui/material/colors";
+import { IndeterminateCheckBox, AddBox, Delete } from "@mui/icons-material";
 
 const Cart = () => {
   const { setProduct } = useContext(MyContext);
   const { inCart, setInCart } = useContext(MyContext);
+
   const [cartTotal, setCartTotal] = useState(0);
 
   const renderCartItems = () => {
@@ -54,9 +55,6 @@ const Cart = () => {
                     {item.displayName}
                   </Typography>
                 </Link>
-                {/* <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {item.description}
-                          </Typography> */}
                 <Typography variant="h5" component="div">
                   &euro;{item.currentPrice}
                 </Typography>
@@ -85,7 +83,7 @@ const Cart = () => {
                       );
                     }}
                   >
-                    <IndeterminateCheckBoxIcon />
+                    <IndeterminateCheckBox />
                   </IconButton>
                   <Typography>{item.amount}</Typography>
                   <IconButton
@@ -104,7 +102,7 @@ const Cart = () => {
                       );
                     }}
                   >
-                    <AddBoxIcon />
+                    <AddBox />
                   </IconButton>
                 </div>
 
@@ -121,123 +119,11 @@ const Cart = () => {
                     );
                   }}
                 >
-                  <DeleteIcon />
+                  <Delete />
                 </IconButton>
               </CardActions>
-
-              {/* <button
-                onClick={() => {
-                  setInCart(
-                    inCart.map((el) => {
-                      if (el.itemId === item.itemId && el.size === item.size) {
-                        if (item.amount > 1) item.amount -= 1;
-                        return el;
-                      } else return el;
-                    })
-                  );
-                }}
-              >
-                -
-              </button>
-              <button
-                onClick={() => {
-                  setInCart(
-                    inCart.map((el) => {
-                      if (el.itemId === item.itemId && el.size === item.size) {
-                        if (item.amount < 10) item.amount += 1;
-                        return el;
-                      } else return el;
-                    })
-                  );
-                }}
-              >
-                +
-              </button>
-              <button
-                onClick={() => {
-                  setInCart(
-                    inCart.filter((el) => {
-                      return (
-                        el.itemId !== item.itemId ||
-                        (el.itemId === item.itemId && el.size !== item.size)
-                      );
-                    })
-                  );
-                }}
-              >
-                remove
-              </button> */}
             </CardContent>
           </Card>
-
-          {/* <p>
-            {item.displayName} <span> - {item.size}</span>{" "}
-            <span> x {item.amount}</span>
-          </p> */}
-
-          {/* <input
-            type="number"
-            name=""
-            id=""
-            value={item.amount}
-            min="1"
-            max="10"
-            onChange={(e) => {
-              if (e.target.value > 0 && e.target.value < 11) {
-                setInCart(
-                  inCart.map((el) => {
-                    if (el.itemId === item.itemId) {
-                      el.amount = Number(e.target.value);
-                      return el;
-                    }
-                  })
-                );
-              }
-            }}
-          /> */}
-
-          {/* <button
-            onClick={() => {
-              setInCart(
-                inCart.map((el) => {
-                  if (el.itemId === item.itemId && el.size === item.size) {
-                    if (item.amount > 1) item.amount -= 1;
-                    return el;
-                  } else return el;
-                })
-              );
-            }}
-          >
-            -
-          </button>
-          <button
-            onClick={() => {
-              setInCart(
-                inCart.map((el) => {
-                  if (el.itemId === item.itemId && el.size === item.size) {
-                    if (item.amount < 10) item.amount += 1;
-                    return el;
-                  } else return el;
-                })
-              );
-            }}
-          >
-            +
-          </button>
-          <button
-            onClick={() => {
-              setInCart(
-                inCart.filter((el) => {
-                  return (
-                    el.itemId !== item.itemId ||
-                    (el.itemId === item.itemId && el.size !== item.size)
-                  );
-                })
-              );
-            }}
-          >
-            remove
-          </button> */}
         </div>
       );
     });
@@ -278,7 +164,6 @@ const Cart = () => {
           <Card
             sx={{
               width: "320px",
-              // height: "188px",
               backgroundColor: "rgba(255, 215, 0, 0.5)",
               backgroundColor: "pink",
               backgroundColor: purple[100],
